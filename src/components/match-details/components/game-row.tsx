00e20e2bc650c978b88dff08e2152abdfc5cb0c5
ahmednkhan24 <ahmednkhan24@gmail.com>
2024-06-22 19:06:15 -0500

    game row

---
+++ src/components/match-details/components/game-row.tsx
@@ -0,0 +1,46 @@
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Row from 'react-bootstrap/Row';
+
+export interface PlayerCardProps {
+  players: Player[];
+}
+
+function PlayerCard({ players }: PlayerCardProps) {
+  return (
+    <Card onClick={() => console.log('clicked')}>
+      <Card.Body>
+        <ListGroup variant="flush">
+          {players.map((player) => (
+            <ListGroup.Item key={player.id}>{player.name}</ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export interface GameRowProps {
+  game: Game;
+  gameNum: number;
+}
+
+export function GameRow({ game, gameNum }: GameRowProps) {
+  return (
+    <div className="pb-3">
+      <Card.Title className="pb-2">Game {gameNum}</Card.Title>
+      <Row className="align-items-center justify-content-space-between text-center">
+        <Col xs={5}>
+          <PlayerCard players={game.team1.players} />
+        </Col>
+        <Col xs={2}>
+          <h2>vs</h2>
+        </Col>
+        <Col xs={5}>
+          <PlayerCard players={game.team2.players} />
+        </Col>
+      </Row>
+    </div>
+  );
+}
--- src/components/match-details/match-details.tsx
+++ src/components/match-details/match-details.tsx
@@ -1,9 +1,5 @@
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Row from 'react-bootstrap/Row';
+import { GameRow } from './components/game-row';
 import { useGenerateMatch } from './hooks/use-generate-match';
-import styles from './match-details.module.scss';
 
 export interface MatchDetailsProps {
   players: Player[];
@@ -20,79 +16,8 @@ export function MatchDetails({
 
   console.log('games: ', games);
 
-  function PlayerName({ player }: { player: Player }) {
-    return (
-      <Card.Text className={styles.playerName}>
-        {JSON.stringify(player)}
-      </Card.Text>
-    );
-  }
-
-  function PlayerNames({ players }: { players: Player[] }) {
-    return (
-      <>
-        {players.map((player) => (
-          <PlayerName key={player.id} player={player} />
-        ))}
-      </>
-    );
-  }
-
-  function Team({ team }: { team: Team }) {
-    return (
-      <Card onClick={() => console.log('clicked')}>
-        <Card.Body>
-          <PlayerNames players={team.players} />
-        </Card.Body>
-      </Card>
-    );
-  }
-
-  function GameRow({ game, gameNum }: { game: Game; gameNum: number }) {
-    const { team1, team2 } = game;
-
-    return (
-      <div>
-        <Card.Title>Game {gameNum}</Card.Title>
-        <div className={styles.gameRow}>
-          <Team team={team1} />
-          <span>vs</span>
-          <Team team={team2} />
-        </div>
-      </div>
-    );
-  }
-
   return (
     <>
-      <div className="pb-3">
-        <Card.Title className="pb-2">Test</Card.Title>
-        <Row className="align-items-center justify-content-space-between text-center">
-          <Col xs={5}>
-            <Card onClick={() => console.log('clicked')}>
-              <Card.Body>
-                <ListGroup variant="flush">
-                  <ListGroup.Item>Player 1</ListGroup.Item>
-                  <ListGroup.Item>Player 2</ListGroup.Item>
-                </ListGroup>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col xs={2}>
-            <h2>vs</h2>
-          </Col>
-          <Col xs={5}>
-            <Card onClick={() => console.log('clicked')}>
-              <Card.Body>
-                <ListGroup variant="flush">
-                  <ListGroup.Item>Player 3</ListGroup.Item>
-                  <ListGroup.Item>Player 4</ListGroup.Item>
-                </ListGroup>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </div>
       {games.map((game, idx) => (
         <GameRow key={game.id} game={game} gameNum={idx + 1} />
       ))}
