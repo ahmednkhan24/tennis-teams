851ed65fc6cfcc0d8f87da6d5f8a3375b10a489d
ahmednkhan24 <ahmednkhan24@gmail.com>
2024-06-22 19:11:10 -0500

    align match type

--- src/components/match-type/match-type.tsx
+++ src/components/match-type/match-type.tsx
@@ -1,6 +1,8 @@
 import { Dispatch, ReactNode, SetStateAction } from 'react';
 import { CheckCircle, PeopleFill, PersonFill } from 'react-bootstrap-icons';
+import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Row from 'react-bootstrap/Row';
 import styles from './match-type.module.scss';
 
 interface ItemProps {
@@ -12,18 +14,20 @@ interface ItemProps {
 
 function Item({ text, selected, startElement, onClick }: ItemProps) {
   return (
-    <ListGroup.Item
-      action
-      variant={selected ? 'success' : 'light'}
-      onClick={onClick}
-      className={styles.item}
-    >
-      <span className={styles.itemContent}>
-        {startElement}
-        <h1 className={styles.matchTypeHeading}>{text}</h1>
-        {selected && <CheckCircle size={35} />}
-      </span>
-    </ListGroup.Item>
+    <Col md={6} sm={12}>
+      <ListGroup.Item
+        action
+        variant={selected ? 'success' : 'light'}
+        onClick={onClick}
+        className={styles.item}
+      >
+        <span className={styles.itemContent}>
+          {startElement}
+          <h1 className={styles.matchTypeHeading}>{text}</h1>
+          {selected && <CheckCircle size={35} />}
+        </span>
+      </ListGroup.Item>
+    </Col>
   );
 }
 
@@ -36,18 +40,20 @@ export function MatchType({ matchType, setMatchType }: MatchTypeProps) {
   return (
     <>
       <h2 className="text-center pt-4">What type of match are you playing?</h2>
-      <Item
-        text="Singles"
-        startElement={<PersonFill size={50} />}
-        selected={matchType === 'singles'}
-        onClick={() => setMatchType('singles')}
-      />
-      <Item
-        text="Doubles"
-        startElement={<PeopleFill size={50} />}
-        selected={matchType === 'doubles'}
-        onClick={() => setMatchType('doubles')}
-      />
+      <Row className="pt-3">
+        <Item
+          text="Singles"
+          startElement={<PersonFill size={50} />}
+          selected={matchType === 'singles'}
+          onClick={() => setMatchType('singles')}
+        />
+        <Item
+          text="Doubles"
+          startElement={<PeopleFill size={50} />}
+          selected={matchType === 'doubles'}
+          onClick={() => setMatchType('doubles')}
+        />
+      </Row>
     </>
   );
 }
